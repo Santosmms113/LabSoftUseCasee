5b63f0bca771745fbcf00f91db486748a9b8fc6a
REGINALDO REIS DE SANTANA <113715701+reginaldosenai@users.noreply.github.com>
2026-08-14 15:56:53 -0300

    Regra tarefa data implementad

--- SLNTask/AppTask/Controllers/TarefaController.cs
+++ SLNTask/AppTask/Controllers/TarefaController.cs
@@ -6,16 +6,19 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using AppTask.Models;
+using AppTask.Models.Services;
 
 namespace AppTask.Controllers
 {
     public class TarefaController : Controller
     {
         private readonly DbTasksContext _context;
+        private RegraTarefa _regraTarefa;
 
         public TarefaController(DbTasksContext context)
         {
             _context = context;
+            _regraTarefa = new RegraTarefa();
         }
 
         // GET: Tarefa
@@ -66,13 +69,16 @@ namespace AppTask.Controllers
         {
             if (ModelState.IsValid)
             {
-                _context.Add(tarefa);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
+                    _context.Add(tarefa);
+                    await _context.SaveChangesAsync();
+                    return RedirectToAction(nameof(Index));
+
+
             }
             ViewData["ListaFuncionario"] = new SelectList(_context.Funcionarios, "Codigo", "Nome", tarefa.FuncionarioId);
             return View(tarefa);
         }
+
 
         // GET: Tarefa/Edit/5
         public async Task<IActionResult> Edit(int? id)
@@ -87,7 +93,7 @@ namespace AppTask.Controllers
             {
                 return NotFound();
             }
-            ViewData["CodigoFuncionario"] = new SelectList(_context.Funcionarios, "Codigo", "Nome", tarefa.FuncionarioId);
+            ViewData["FuncionarioId"] = new SelectList(_context.Funcionarios, "Codigo", "Nome", tarefa.FuncionarioId);
             return View(tarefa);
         }
 
@@ -96,14 +102,14 @@ namespace AppTask.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Codigo,Descricao,DataPlanejada,DataIniciada,DataFinalizada,DataCancelada,StatusTarefa,Prazo,CodigoFuncionario")] Tarefa tarefa)
+        public async Task<IActionResult> Edit(int id, [Bind("Codigo,Descricao,DataPlanejada,DataIniciada,DataFinalizada,DataCancelada,StatusTarefa,Prazo,FuncionarioId")] Tarefa tarefa)
         {
             if (id != tarefa.Codigo)
             {
                 return NotFound();
             }
 
-            if (ModelState.IsValid)
+            if (ModelState.IsValid & _regraTarefa.validarDataFinal(tarefa.DataIniciada, tarefa.DataFinalizada))
             {
                 try
                 {
@@ -123,7 +129,7 @@ namespace AppTask.Controllers
                 }
                 return RedirectToAction(nameof(Index));
             }
-            ViewData["CodigoFuncionario"] = new SelectList(_context.Funcionarios, "Codigo", "Nome", tarefa.FuncionarioId);
+            ViewData["FuncionarioId"] = new SelectList(_context.Funcionarios, "Codigo", "Nome", tarefa.FuncionarioId);
             return View(tarefa);
         }
 
