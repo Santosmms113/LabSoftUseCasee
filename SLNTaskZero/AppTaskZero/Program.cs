01d4af3ad6e147a0d17217f2e29a61c603fc4639
REGINALDO REIS DE SANTANA <113715701+reginaldosenai@users.noreply.github.com>
2026-08-28 09:41:49 -0300

    crud funcionario

--- SLNTaskZero/AppTaskZero/Program.cs
+++ SLNTaskZero/AppTaskZero/Program.cs
@@ -1,7 +1,13 @@
+using AppTaskZero.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<DbTasksZeroContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("ConexaoSqlServer")));
 
 var app = builder.Build();
 
